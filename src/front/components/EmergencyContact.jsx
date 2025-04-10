86de8f59f32ab49f6cf473df925defef67227c5a
MichPisani <michelinapisani@gmail.com>
2025-04-10 12:27:13 +0000

    dashboard con props

--- src/front/components/EmergencyContact.jsx
+++ src/front/components/EmergencyContact.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const EmergencyContact = () => {
+const EmergencyContact = ({lastEmergencyContact}) => {
     return (
         <div className="card h-100">
             <div className="card-header bg-primary text-white">
                 <h5 className="card-title mb-0">Contacto de emergencia</h5>
             </div>
             <div className="card-body">
-                <div className="d-flex justify-content-center"><p>Pendiente contenido contacto emergencia</p></div>
+
+                {lastEmergencyContact && lastEmergencyContact.first_name_contact ?
+                    <div className="d-flex justify-content-center">
+                        <h2>{`${lastEmergencyContact.first_name_contact} ${lastEmergencyContact.last_name_contact}`}</h2>
+                        <p>{`Teléfono: ${lastEmergencyContact.phone_contact}`}</p>
+                        <p>{`Email: ${lastEmergencyContact.email_contact}`}</p>
+                    </div>
+                    :
+                    <span className="badge bg-secondary">No ha resgistrado ningún contacto</span>
+                }
+                
             </div>
         </div>
     )
--- src/front/pages/Dashboard.jsx
+++ src/front/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { LayoutPrivate } from "./LayoutPrivate";
 import RecentRecords from "../components/RecentRecords";
@@ -8,6 +8,60 @@ import EmergencyContact from "../components/EmergencyContact";
 
 
 const Dashboard = () => {
+
+    const [dashboardInfo, setDashboardInfo] = useState({
+        lastBloodPressure: {},
+        lastGlucose: {},
+        lastWeight: {},
+        lastEmergencyContact: {},
+        currentMedication: [{medication_name: "paracetamol", dosage_instructions: "Una por dia"}]
+    })
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+
+        const fetchInfo = async () => {
+            const backendUrl = import.meta.env.VITE_BACKEND_URL || "";
+
+            const accessToken = localStorage.getItem("accessToken")
+
+            try {
+                setIsLoading(true)
+                const response = await fetch(`${backendUrl}/api/dashboard`,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer " + accessToken
+                        }
+                    });
+
+                const data = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(data.error || "Error al acceder al area privada");
+                }
+
+                setDashboardInfo({
+                    lastBloodPressure: data.last_blood_pressure || undefined,
+                    lastGlucose: data.last_glucose || undefined,
+                    lastWeight: data.last_weight || undefined,
+                    lastEmergencyContact: data.last_emergency_contact || undefined,
+                    currentMedication: data.current_medication || []
+                })
+
+            } catch (error) {
+                setError(error.message)
+
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchInfo()
+
+    }, [])
+
     return (
         <>
             {/*<LayoutPrivate />*/}
@@ -15,21 +69,34 @@ const Dashboard = () => {
             <div className="container">
                 <h1 className="mb-4 mt-4">Bienvenido a CuidaMed Usuario</h1>
 
-                <div className="row g-4">
-                    <div className="col-md-6">
-                        <RecentRecords/>
-                    </div>
+                {isLoading ? (
+                    <>
+                        <span className="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>
+                        Cargando area privada...
+                    </>
+                ) : (
+                    <div className="row g-4">
+                        <div className="col-md-6">
+                            <RecentRecords
+                                lastBloodPressure={dashboardInfo.lastBloodPressure}
+                                lastGlucose={dashboardInfo.lastGlucose}
+                                lastWeight={dashboardInfo.lastWeight}
+                            />
+                        </div>
 
-                    <div className="col-md-6">
-                        <WeightStatistics/>
+                        <div className="col-md-6">
+                            <WeightStatistics />
+                        </div>
+                        <div className="col-md-6">
+                            <CurrentMedication currentMedication={dashboardInfo.currentMedication} />
+                        </div>
+                        <div className="col-md-6">
+                            <EmergencyContact lastEmergencyContact={dashboardInfo.lastEmergencyContact} />
+                        </div>
                     </div>
-                    <div className="col-md-6">
-                        <CurrentMedication/>
-                    </div>
-                    <div className="col-md-6">
-                        <EmergencyContact/>
-                    </div>
-                </div>
+                )}
+
+
             </div>
         </>
     )
