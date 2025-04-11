de7791223715f15fa21613f6b689fe218121ee9f
Leodelis <leodelis19@gmail.com>
2025-04-11 08:12:50 +0000

    inicio metodo POST contacto de emergencia

--- src/api/records/__init__.py
+++ src/api/records/__init__.py
@@ -1,15 +1,17 @@
 from flask import Blueprint
 
 from .blood_pressure import blood_pressure_blueprint
+from .emergency_contact import emergency_contact_blueprint
 from .glucose import glucose_blueprint
 from .height import height_blueprint
 from .weight import weight_blueprint
-
 
 records_blueprint = Blueprint('records', __name__)
 
 records_blueprint.register_blueprint(
     blood_pressure_blueprint, url_prefix='/bloodpressure')
+
+records_blueprint.register_blueprint(emergency_contact_blueprint, url_prefix='/emergency_contact')
 
 records_blueprint.register_blueprint(glucose_blueprint, url_prefix='/glucose')
 
