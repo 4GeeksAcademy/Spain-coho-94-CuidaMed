9136577835db42fd1f7d5e11d47ea3c77a394ed4
Leodelis <leodelis19@gmail.com>
2025-04-14 13:24:00 +0000

    Ruta antecedentes-familiares metodo POST funcionando

--- src/api/records/medical_history.py
+++ src/api/records/medical_history.py
@@ -3,8 +3,51 @@ En este archivo están todas las rutas de Antecedentes Familiare
 Ruta /api/records/medical_history
 """
 from flask import request, jsonify, Blueprint
-from api.models import db, MedicalHistory
+from api.models import db, User, MedicalHistory
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from datetime import datetime
 
 medical_history_blueprint = Blueprint('medical_history', __name__)
+
+
+@medical_history_blueprint.route('/', methods=['POST'])
+@jwt_required()
+def create_medical_history():
+    try:
+        current_user_id = get_jwt_identity()
+
+        user = User.query.get(current_user_id)
+        if not user:
+            return jsonify({"msg": "Usuario no encontrado"}), 404
+
+        data = request.get_json()
+
+        # Verificar campo requerido parentesco madre/padre
+        if 'kinship' not in data:
+            return jsonify({"msg": "Falta el campo parentesco madre/padre"}), 400
+
+        # Verificar campo requerido enfermedad
+        if 'disease' not in data:
+            return jsonify({"msg": "Falta el campo escribir enfermedad"}), 400
+
+        # Crear nuevo registro de antecedentes familiares
+        new_medical_history = MedicalHistory(
+            user_id=current_user_id,
+            kinship=data['kinship'],
+            disease=data['disease']
+        )
+
+        db.session.add(new_medical_history)
+        db.session.commit()
+
+        # No devuelvo registration_date porque no es necesario
+        return jsonify({
+            'message': "Antecedentes familiares creado con éxito",
+            'id': new_medical_history.id,
+            'kinship': new_medical_history.kinship,
+            'disease': new_medical_history.disease
+        }), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": "Error al guardar el registro antecedentes familiares", "error": str(e)}), 500
