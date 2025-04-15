7ff048ea4c081105e4389494f211981b816fdf0b
MichPisani <michelinapisani@gmail.com>
2025-04-15 11:19:50 +0000

    eliminando botones del navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -4,13 +4,12 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-light border-bottom">
       <div className="container">
-        {/* Logo y marca */}
-        <a className="navbar-brand d-flex align-items-center" href="#">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
           <img
             src="src/front/assets/img/LogoNavbar.png"
             style={{ width: "150px" }}
           />
-        </a>
+        </Link>
 
         {/* Botón de colapso para móviles */}
         <button
@@ -27,31 +26,7 @@ export const Navbar = () => {
 
         {/* Contenido colapsable */}
         <div className="collapse navbar-collapse" id="navbarContent">
-          {/* Links de navegación */}
-          <ul className="navbar-nav mx-auto mb-2 mb-md-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#features">
-                Boton1
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#how-it-works">
-                Boton2
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#testimonials">
-                Boton3
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                Boton4
-              </a>
-            </li>
-          </ul>
 
-          {/* Botones de acción */}
           <div className="d-flex align-items-center">
             <Link
               to="/login"
