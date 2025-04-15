bb36ea9d03667d3ff7ad3a6f55f30d2b286eb2f6
Leodelis <leodelis19@gmail.com>
2025-04-15 08:56:42 +0000

    Ruta antecedentes-personales metodo POST funcionando

--- src/api/records/personal_antecedents.py
+++ src/api/records/personal_antecedents.py
@@ -1,6 +1,7 @@
 """
 En este archivo están todas las rutas para antecedentes personales
 Ruta /api/records/personal_antecedents
+Metodo POST - GET - DELETE
 """
 from flask import request, jsonify, Blueprint
 from api.models import db, User, PersonalAntecedent
@@ -8,3 +9,39 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from datetime import datetime
 
 personal_antecedents_blueprint = Blueprint('personal_antecedents', __name__)
+
+@personal_antecedents_blueprint.route('/', methods=['POST'])
+@jwt_required()
+def create_personal_antecedents():
+        current_user_id = get_jwt_identity()
+
+        user = User.query.get(current_user_id)
+        if not user:
+            return jsonify({"msg": "Usuario no encontrado"}), 404
+
+        data = request.get_json()
+
+         # Verificar datos obligatorios (el usuario debe escribir si padece alguna enfermedad base)
+        if 'disease' not in data:
+            return jsonify({"msg": "El nombre de la enfermedad es obligatorio"}), 400
+    
+        # Crear nuevo antecedente personal
+        new_personal_antecedent = PersonalAntecedent(
+            user_id=current_user_id,
+            disease=data['disease']    
+        )
+    
+        # Procesar la fecha si el usuario la introduce (no es obligatorio)
+        if 'diagnosis_date' in data and data['diagnosis_date']:
+            try:
+                new_personal_antecedent.diagnosis_date = datetime.strptime(data['diagnosis_date'], "%d-%m-%Y")
+            except ValueError:
+                return jsonify({"msg": "Formato de fecha inválido"}), 400
+
+        db.session.add(new_personal_antecedent)
+        db.session.commit()
+        
+        return jsonify(new_personal_antecedent.serialize_personal_antecedent()), 201
+
+
+        
