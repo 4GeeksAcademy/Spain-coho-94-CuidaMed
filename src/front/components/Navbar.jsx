5ac0148657bc961b3f78579e43e08e0407eb4508
MichPisani <michelinapisani@gmail.com>
2025-04-20 22:04:05 +0000

    Retoques para que sea responsive

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -4,42 +4,26 @@ import navicon from "/src/front/assets/img/LogoNavbar.png"
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-light border-bottom">
-      <div className="container">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
         <Link className="navbar-brand d-flex align-items-center" to="/">
           <img
             src={navicon}
             style={{ width: "150px" }}
           />
         </Link>
-
-        {/* Botón de colapso para móviles */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarContent"
-          aria-controls="navbarContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fa-solid fa-bars"></i>
-        </button>
-
-        {/* Contenido colapsable */}
-        <div className="collapse navbar-collapse" id="navbarContent">
-
-          <div className="ms-auto d-flex align-items-center gap-3">
-            <Link
-              to="/login"
-              className="text-decoration-none"
-            >
-              Accede
-            </Link>
-            <Link to="/signup" className="btn btn-primary">
-              Regístrate
-            </Link>
-          </div>
+        
+        <div className="ms-auto d-flex align-items-center gap-3">
+          <Link
+            to="/login"
+            className="text-decoration-none"
+          >
+            Accede
+          </Link>
+          <Link to="/signup" className="btn btn-primary">
+            Regístrate
+          </Link>
         </div>
+        
       </div>
     </nav>
   );
